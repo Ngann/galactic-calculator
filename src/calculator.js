696c93dcbdc92b1f8eb6f5286b856095dfbeb07d
Student <example@example.com>
2019-01-11 14:48:53 -0800

    add function yearsToDays

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,23 @@
-class solarAge{
+class solarAge {
+  constructor (age){
+  this.age = age;
+}
+
+  yearsToDays(){
+    return this.age * 365
+  }
+
+  convertAge(planet){
+  const daysEarth = 365;
+  const mercury = .24;
+  const venus = .62;
+  const mars = 1.88;
+  const jupiter = 11.86;
+  // const yearsToDays = this.age * 365;
+  const planetToEarth = daysEarth * planet ;
+
+  return (yearsTodays()/planetToEarth);
+  }
 
 }
 
