d4a0555802002d317c9a7340d0000630f76e987e
Student <example@example.com>
2019-01-11 13:21:10 -0800

    files/ folder / testspecs

---
+++ spec/calculator-spec.js
@@ -0,0 +1,46 @@
+import { solarAge } from './../src/calculator.js';
+
+describe('solarAge', function () {
+
+  it('will take Earth age and convert it to Mercury age', function() {
+    var ageEarth = 25;
+    var planet = "Mercury";
+    const solarAge = new solarAge(ageEarth);
+    var newAge= solarAge.convertAge(planet);
+    expect(newAge).toEqual(103);
+  });
+
+  it('will take Earth age and convert it to Venus age', function() {
+    var ageEarth = 25;
+    var planet = "Venus";
+    const solarAge = new solarAge(ageEarth);
+    var newAge= solarAge.convertAge(planet);
+    expect(newAge).toEqual(40);
+  });
+
+  it('will take Earth age and convert it to Mars age', function() {
+    var ageEarth = 25;
+    var planet = "Mars";
+    const solarAge = new solarAge(ageEarth);
+    var newAge= solarAge.convertAge(planet);
+    expect(newAge).toEqual(13);
+  });
+
+  it('will take Earth age and convert it to Jupiter age', function() {
+    var ageEarth = 25;
+    var planet = "Jupiter";
+    const solarAge = new solarAge(ageEarth);
+    var newAge= solarAge.convertAge(planet);
+    expect(newAge).toEqual(3);
+  });
+
+  it('will determing the user life expectacy on Earth', function() {
+    var ageEarth = 25;
+    var planet = "Earth";
+    var avgLifeExpectancy = 80;
+    const solarAge = new solarAge(ageEarth);
+    var yearsLeft= solarAge.yearsToLive(ageEarth, avgLifeExpectancy);
+    expect(yearsLeft).toEqual(55);
+  });
+
+});
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { solarAge } from './../src/calculator.js';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function() {
+
+});
