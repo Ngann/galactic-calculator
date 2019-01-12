d6ba00a4cfd2d5ddb9c601594714bc9e2535cd86
Student <example@example.com>
2019-01-11 18:48:08 -0800

    refactor code and specs, add yearsToLive function

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,58 +1,43 @@
 import { solarAge } from './../src/calculator.js';
 
 describe('solarAge', function () {
+  let ageObj = new solarAge(25);
+  let oldObj = new solarAge(90);
+  let avgLifeExpectancy = 80;
 
-  it('it will take age in years and convert it to Earth days', function() {
-    var ageEarth = 25;
-    const ageObj = new solarAge(ageEarth)
-    var newAge= ageObj.yearsToDays()
-    expect(newAge).toEqual(9125)
+  it('will take age in years and convert it to Earth days', function() {
+    var newAge= ageObj.yearsToDays();
+    expect(newAge).toEqual(9125);
   });
 
   it('will take Earth age and convert it to Mercury age', function() {
-    var ageEarth = 25;
-    const ageObj = new solarAge(ageEarth);
     var newAge = ageObj.mercuryAge();
     expect(newAge).toEqual(104);
   });
 
   it('will take Earth age and convert it to Venus age', function() {
-    var ageEarth = 25;
-    const ageObj = new solarAge(ageEarth);
     var newAge = ageObj.venusAge();
     expect(newAge).toEqual(40);
   });
 
   it('will take Earth age and convert it to Mars age', function() {
-    var ageEarth = 25;
-    const ageObj = new solarAge(ageEarth);
     var newAge = ageObj.marsAge();
     expect(newAge).toEqual(13);
   });
 
   it('will take Earth age and convert it to Jupiter age', function() {
-    var ageEarth = 25;
-    const ageObj = new solarAge(ageEarth);
     var newAge = ageObj.jupiterAge();
     expect(newAge).toEqual(2);
   });
 
-      // it('will determine the user life expectancy on Earth', function() {
-      //   var ageEarth = 25;
-      //   var planet = "Earth";
-      //   var avgLifeExpectancy = 80;
-      //   const solarAge = new solarAge(ageEarth);
-      //   var yearsLeft= solarAge.yearsToLive(ageEarth, avgLifeExpectancy);
-      //   expect(yearsLeft).toEqual(55);
-      // });
+  it('will determine the user life expectancy on Earth', function() {
+    var yearsLeft= ageObj.yearsToLive(avgLifeExpectancy);
+    expect(yearsLeft).toEqual('55 years to go!');
+  });
 
-      // it('will determine the user life expectacy on Earth', function() {
-      //   var ageEarth = 88;
-      //   var planet = "Earth";
-      //   var avgLifeExpectancy = 80;
-      //   const solarAge = new solarAge(ageEarth);
-      //   var yearsLeft= solarAge.yearsToLive(ageEarth, avgLifeExpectancy);
-      //   expect(yearsLeft).toEqual("You have surpase the average life expectancy of your peers.");
-      // });
+  it('will determine the user life exceed lide expectancy', function() {
+    var yearsLeft= oldObj.yearsToLive(avgLifeExpectancy);
+    expect(yearsLeft).toEqual("You have surpase the average life expectancy of your peers.");
+  });
 
-    });
+});
--- src/calculator.js
+++ src/calculator.js
@@ -2,38 +2,44 @@ class solarAge {
   constructor (age){
     this.age = age;
   }
+
   yearsToDays(){
     return this.age * 365;
   }
 
   mercuryAge() {
     const mercury = .24;
-    const mercuryDays = mercury * 365;
-    let age = (this.age * 365) / mercuryDays;
-    return Math.round(age)
+    let age = this.age / mercury;
+    return Math.round(age);
   }
 
   venusAge() {
     const venus = .62;
-    const venusDays = venus * 365;
-    let age = (this.age * 365) / venusDays;
-    return Math.round(age)
+    let age = this.age / venus;
+    return Math.round(age);
   }
 
   marsAge() {
     const mars = 1.88;
-    const marsDays = mars * 365;
-    let age = (this.age * 365) / marsDays;
-    return Math.round(age)
+    let age = this.age/ mars;
+    return Math.round(age);
   }
 
   jupiterAge() {
     const jupiter = 11.86;
-    const jupiterDays = jupiter * 365;
-    let age = (this.age * 365) / jupiterDays;
-    return Math.round(age)
+    let age = this.age/ jupiter;
+    return Math.round(age);
+  }
+
+  yearsToLive(avgLifeExpectancy) {
+    let life = avgLifeExpectancy - this.age;
+
+    if (this.age < avgLifeExpectancy) {
+      return life + " " + "years to go!";
+    } else {
+    return  "You have surpase the average life expectancy of your peers.";
+    }
   }
 
 }
-
 export { solarAge };
