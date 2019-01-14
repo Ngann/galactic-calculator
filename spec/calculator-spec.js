14885ace6276507677381d982deb803cdf23ec17
Student <example@example.com>
2019-01-13 16:32:36 -0800

    change var to lets and const

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -18,32 +18,32 @@ describe('Age', function () {
   });
 
   it('will take Earth age and convert it to Mercury age', function() {
-    var newAge = ageObj.mercuryAge();
+    let newAge = ageObj.mercuryAge();
     expect(newAge).toEqual(104);
   });
 
   it('will take Earth age and convert it to Venus age', function() {
-    var newAge = ageObj.venusAge();
+    let newAge = ageObj.venusAge();
     expect(newAge).toEqual(40);
   });
 
   it('will take Earth age and convert it to Mars age', function() {
-    var newAge = ageObj.marsAge();
+    let newAge = ageObj.marsAge();
     expect(newAge).toEqual(13);
   });
 
   it('will take Earth age and convert it to Jupiter age', function() {
-    var newAge = ageObj.jupiterAge();
+    let newAge = ageObj.jupiterAge();
     expect(newAge).toEqual(2);
   });
 
   it('will determine the user life expectancy on Earth', function() {
-    var yearsLeft= ageObj.yearsToLive(avgLifeExpectancy);
+    let yearsLeft= ageObj.yearsToLive(avgLifeExpectancy);
     expect(yearsLeft).toEqual('55 years to go!');
   });
 
   it('will determine the user has lived pass the life expectancy', function() {
-    var yearsLeft= oldObj.yearsToLive(avgLifeExpectancy);
+    let yearsLeft= oldObj.yearsToLive(avgLifeExpectancy);
     expect(yearsLeft).toEqual("You have surpase the average life expectancy of your peers by 10 years!");
   });
 
